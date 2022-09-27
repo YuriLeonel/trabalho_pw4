8f834cedda30c4fbfedf893629e9938da8b62148
Eduardo <educastro100@gmail.com>
2022-09-27 17:26:40 -0300

    REst messages e conexao com o banco

--- server/src/index.js
+++ server/src/index.js
@@ -3,14 +3,20 @@ const app = express();
 
 const port = 8888;
 
-const i18n = require('i18n')
+//const i18n = require('i18n')
 
-i18n.configure({
-  locales: ['pt-BR'],
-  defaultLocale: "pt-BR",
-        directory: "./locales",
-        extension: ".json",
-        cookie: "lang"
+
+app.use(express.json())
+app.use("/contas", require("./conta/indexConta"));
+
+// i18n.configure({
+//   locales: ['pt-BR'],
+//   defaultLocale: "pt-BR",
+//         directory: "./locales",
+//         extension: ".json",
+//         cookie: "lang"
+// })
+
+app.listen(port, function () {
+  console.log(`Servidor iniciado na porta ${port}`);
 })
-
-app.listen(port)
